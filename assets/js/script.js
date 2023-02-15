d1b3fbdc29d2b3bd63a3fa08b4c76aec5e4050d4
madelenviola <Madelen.stenlunds@gmail.com>
2023-02-15 20:25:35 +0000

    change from let to var jshint recommendation and indent js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
 function newElement() {
-    let li = document.createElement('li');
-    let taskValue = document.getElementById('addtaskField').value;
-    let task = document.createTextNode(taskValue);
+    var li = document.createElement('li');
+    var taskValue = document.getElementById('addtaskField').value;
+    var task = document.createTextNode(taskValue);
     li.appendChild(task);
     if (taskValue === '') {
-        alert('Write a task for your to-do list')
+        alert('Write a task for your to-do list');
     } else {
         document.getElementById("todoList").appendChild(li);
     }
     document.getElementById("addtaskField").value = "";
-    const trashCan = document.createElement("span");
-    trashCan.className = ("fa-solid fa-trash");
-    trashCan.onclick = function(){
-    this.parentElement.remove();
-    }
-    li.appendChild(trashCan);
-    
+
+    li.appendChild(addTrashCan());
+    var checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    li.appendChild(checkBox);
+
 }
+
+function addTrashCan() {
+    var trashCan = document.createElement("span");
+    trashCan.className = ("fa-solid fa-trash");
+    trashCan.onclick = function () {
+        this.parentElement.remove();
+    };
+    return trashCan;
+}
+
