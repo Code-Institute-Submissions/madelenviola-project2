375227b4542a29dbae1072e8b67ade539a5026fc
madelenviola <Madelen.stenlunds@gmail.com>
2023-02-15 13:19:07 +0000

    add js to input field enable input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-const form = document.getElementById('to-do-form');
-const taskInput = document.getElementById('add-task-field');
-const tasks = document.getElementById('add-task-submit');
+function newElement(){
+    let li = document.createElement('li');
+    let taskValue = document.getElementById('addtaskField').value;
+    let task = document.createTextNode(taskValue);
+    li.appendChild(task);
+    if (taskValue === '') {
+        alert('Write a task for your to-do list')
+    } else {
+        document.getElementById("todoList").appendChild(li);
+    }
+    document.getElementById("addtaskField").value = "";
 
+        for (i = 0; i > close.length; i++) {
+            close[i].onclick= function(){
+                let div = this.parentElement;
+                div.style.display = 'none';
+        }
+    }
+}
